118f1ab2b6357e23d6154a34593f1c2ff5b97854
m990 <League@AdministorsiMac.attlocal.net>
2018-04-16 20:03:13 -0700

    Todolist

--- src/Todo.java
+++ src/Todo.java
@@ -1,5 +1,10 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
@@ -32,6 +37,22 @@ public class Todo implements ActionListener {
 		td.frame.add(td.panel);
 		td.frame.setVisible(true);
 		td.frame.setSize(200, 100);
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("src/todo.txt"));
+			String line = br.readLine();
+			while (line != null) {
+				td.todoList.add(line);
+				System.out.println(line);
+				line = br.readLine();
+			}
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		JOptionPane.showMessageDialog(null, td.todoList);
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -43,12 +64,75 @@ public class Todo implements ActionListener {
 		}
 		else if (e.getSource() == remove) {
 			System.out.println("Remove");
+			String item = JOptionPane.showInputDialog("Enter thing to remove:");
+			boolean found = false;
+			String item3 = "";
+			for (String item2: todoList) {
+				System.out.println(item2);
+				if (item2.equals(item)) {
+					found = true;
+					item3 = item2;
+				}
+			}
+			if (found) {
+				System.out.println("found");
+				todoList.remove(item3);
+				System.out.println(todoList);
+			}
 		}
 		else if (e.getSource() == save) {
 			System.out.println("Save");
+			if (todoList.isEmpty()) {
+				try {
+					FileWriter fw = new FileWriter("src/todo.txt");
+					fw.write("");
+					fw.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			else {
+				try {
+					FileWriter fw = new FileWriter("src/todo.txt");
+					fw.write("");
+					fw.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				for (String item: todoList) {
+					try {
+						FileWriter fw = new FileWriter("src/todo.txt", true);
+						System.out.println("Made file writer");
+						System.out.println(item + "\n");
+						fw.write(item + "\n");
+						fw.close();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						JOptionPane.showMessageDialog(null, "Something went wrong");
+					}
+				}
+			}
 		}
 		else if (e.getSource() == load) {
 			System.out.println("Load");
+			for (String item: todoList) {
+				try {
+					BufferedReader br = new BufferedReader(new FileReader("src/todo.txt"));
+					String line = br.readLine();
+					while (line != null) {
+						todoList.add(line);
+						line = br.readLine();
+					}
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
 		}
 	}
 }
